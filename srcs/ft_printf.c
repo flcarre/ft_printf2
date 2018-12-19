1f8d1f5a1e6771944883a33d236fff40d69f0221
flcarre <flcarre@student.42.fr>
2018-12-19 17:09:12 +0100

    push

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lutsiara <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: lutsiara <lutsiara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/12 18:04:14 by lutsiara          #+#    #+#             */
-/*   Updated: 2018/12/12 18:17:50 by lutsiara         ###   ########.fr       */
+/*   Updated: 2018/12/19 17:07:16 by flcarre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 int		ft_printf(const char *format, ...)
 {
+	va_list	args;
+
+	va_start(args, format);
+	ft_parse
+	va_end(args);
 	return ((int)ft_strlen(format));
 }
