09eb72f9e4fe86d8b23e7ce5a2a938a03205986e
flcarre <flcarre@student.42.fr>
2018-12-20 19:37:05 +0100

    fastpush

--- srcs/ft_compid.c
+++ srcs/ft_compid.c
@@ -6,7 +6,7 @@
 /*   By: flcarre <flcarre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/19 18:25:46 by flcarre           #+#    #+#             */
-/*   Updated: 2018/12/20 17:45:37 by flcarre          ###   ########.fr       */
+/*   Updated: 2018/12/20 19:36:56 by flcarre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,14 @@ unsigned long					ft_compid(char *id, t_id **list)
 	e = ft_newid();
 	if (ft_atoui(id))
 	{
-
+		ft_digitlen()
 	}
 	while (!e->w && id[i] && ft_isfm(id[i]))
 	{
 		e->fm |= ft_isfm(id[i]);
 		i++;
 	}
+	ft_isflag(id + i, e->lm, &i);
 	e->id = (ft_isid(id[i])) ? id[i] : '\0';
 	ft_enqueueid(list, e);
 	return (i + 1);
