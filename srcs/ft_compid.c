d5e7c2fe33696141cf46b3feed4807bbbe5e305f
flcarre <flcarre@student.42.fr>
2018-12-20 20:45:00 +0100

    compid

--- srcs/ft_compid.c
+++ srcs/ft_compid.c
@@ -6,7 +6,7 @@
 /*   By: flcarre <flcarre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/19 18:25:46 by flcarre           #+#    #+#             */
-/*   Updated: 2018/12/20 19:36:56 by flcarre          ###   ########.fr       */
+/*   Updated: 2018/12/20 20:39:16 by flcarre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,10 @@ unsigned long					ft_compid(char *id, t_id **list)
 	e = ft_newid();
 	if (ft_atoui(id))
 	{
-		ft_digitlen()
+		e->pos = (id[i + (unsigned long)ft_udigitlen(ft_atoui(id))] == '$') \
+							? ft_atoui(id) : e->pos;
+		i += (unsigned long)ft_udigitlen(ft_atoui(id)) + 1;
+		i += (id[i])
 	}
 	while (!e->w && id[i] && ft_isfm(id[i]))
 	{
