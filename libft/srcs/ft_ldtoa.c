5135baf2e53cd58004225be9d11b11451eba3e62
Lucas TSIARASO <tsiaraso@gmail.com>
2019-01-04 05:25:45 +0100

    update

--- libft/srcs/ft_ldtoa.c
+++ libft/srcs/ft_ldtoa.c
@@ -6,7 +6,7 @@
 /*   By: lutsiara <lutsiara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/14 17:17:57 by lutsiara          #+#    #+#             */
-/*   Updated: 2019/01/04 05:08:11 by lutsiara         ###   ########.fr       */
+/*   Updated: 2019/01/04 05:25:33 by lutsiara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,18 @@ char		*ft_ldtoa(long double x)
 {
 	char			*a;
 	char			*p;
-	unsigned int	i;
+	unsigned int	i[2];
 	t_u_ieee_ld		u;
 
-	i = ft_ld_ldiglen(x, 10.0L);
-	if (!(a = ft_strnew((unsigned long)i + 16445 + 1)))
+	i[0] = ft_ld_ldiglen(x, 10.0L);
+	if (!(a = ft_strnew((unsigned long)i[0] + 16445 + 1)))
 		return ((void *)0);
-	p = a;
+	p = a + i[0] + 16445 + 1;
 	u.ld = x;
-	while (p < a + i + 16445 + 1 && )
+	i[1] = 0;
+	while (p > a && i[1] < LD_FLB)
+	{
+		
+	}
 	return (a);
 }
