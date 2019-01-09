59ff51f710b787474b171088afbc91bb7b3350ef
flcarre <flcarre@student.42.fr>
2019-01-09 18:10:54 +0100

    fastpush

--- srcs/ft_conv.c
+++ srcs/ft_conv.c
@@ -68,16 +68,26 @@ void ft_diouxx(t_id *e)
 }
 */
 
-void ft_diouxx(t_id *e)
+void ft_conv(t_id *e)
 {
+	char *tmp;
+
 	(!ft_strcmp(e->id, "d") || !ft_strcmp(e->id, "i")) ? \
 	ft_strjoin(e->s, ft_lltoa((long long)*(e->a))) : 0;
 	(!ft_strcmp(e->id, "o")) ? \
 	(e->s = ft_ulltoa_base((unsigned long long)*(e->a), "01234567")) : 0;
 	(!ft_strcmp(e->id, "u")) ? \
-	(e->s = ft_ulltoa_base((unsigned long long)*(e->a), "0123456789")) : 0;
+	(e->s = ft_ulltoa((unsigned long long)*(e->a))) : 0;
 	(!ft_strcmp(e->id, "x")) ? (e->s = \
 	ft_ulltoa_base((unsigned long long)*(e->a), "0123456789abcdef")) : 0;
 	(!ft_strcmp(e->id, "X")) ? (e->s = \
 	ft_ulltoa_base((unsigned long long)*(e->a), "0123456789ABCDEF")) : 0;
+	if (!ft_strcmp("p", e->id))
+	{
+		tmp = ft_ulltoa_base((unsigned long long)*(e->a), "0123456789abcdef");
+		e->s = ft_strjoin("0x", tmp);
+		if (!tmp)
+			ft_memdel(e->s);
+		ft_memdel(tmp);
+	}
 }
