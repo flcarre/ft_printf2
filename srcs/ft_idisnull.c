d32e3c29180f20714e5cf2fe18aa3e7a29e11e78
Lucas TSIARASO <tsiaraso@gmail.com>
2019-02-19 13:36:51 +0100

    update

--- srcs/ft_idisnull.c
+++ srcs/ft_idisnull.c
@@ -6,7 +6,7 @@
 /*   By: flcarre <flcarre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/20 17:45:01 by flcarre           #+#    #+#             */
-/*   Updated: 2019/02/14 15:59:13 by lutsiara         ###   ########.fr       */
+/*   Updated: 2019/02/19 13:28:22 by lutsiara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int		ft_idisnull(t_id *e)
 {
-	if (e->id[0] == '%' && (e->lm[0] || e->p || e->w || e->fm))
+	if (e->id[0] == '%' && (e->lm[0] || e->p[0] || e->w || e->fm))
 		return (1);
-	if (e->id[0] == '{' && (e->lm[0] || e->p || e->w || e->fm))
+	if (e->id[0] == '{' && (e->lm[0] || e->p[0] || e->w || e->fm))
 		return (1);
 	if (e->id[0] == 'f' && ft_strcmp("l", e->lm) && ft_strcmp("", e->lm) && \
 	ft_strcmp("L", e->lm))
