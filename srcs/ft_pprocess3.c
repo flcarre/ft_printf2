6a44e1f0e653e74993e3af280c4d4a5b7c44762c
flcarre <flcarre@student.42.fr>
2019-02-26 17:31:50 +0100

    fastpush

--- srcs/ft_pprocess3.c
+++ srcs/ft_pprocess3.c
@@ -6,7 +6,7 @@
 /*   By: lutsiara <lutsiara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/18 16:22:11 by lutsiara          #+#    #+#             */
-/*   Updated: 2019/02/26 15:59:04 by lutsiara         ###   ########.fr       */
+/*   Updated: 2019/02/26 17:29:07 by flcarre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,15 @@ static int	ft_iputfm(t_id *e, unsigned long long x)
 		r[0] += r[1];
 	else if (r[1] >= 0 && (e->fm & 1) == 1 && (r[1] = ft_iputchar('+')) >= 0)
 		r[0] += r[1];
-	if (r[1] >= 0 && ((e->fm & 16) == 16 || e->id[0] == 'p') && x != 0)
+	if (r[1] >= 0 && ((e->fm & 16) == 16 || e->id[0] == 'p'))
 	{
-		if (e->id[0] == 'o' && (r[1] = ft_iputchar('0')) >= 0)
+		if (e->id[0] == 'o' && x != 0 && (r[1] = ft_iputchar('0')) >= 0)
 			r[0] += r[1];
-		else if (r[1] >= 0 && (e->id[0] == 'x' || e->id[0] == 'p') && \
-		(r[1] = ft_iputstr("0x")) >= 0)
+		else if (r[1] >= 0 && ((e->id[0] == 'x'  && x != 0) || \
+		e->id[0] == 'p') && (r[1] = ft_iputstr("0x")) >= 0)
 			r[0] += r[1];
-		else if (r[1] >= 0 && e->id[0] == 'X' && (r[1] = ft_iputstr("0X")) >= 0)
+		else if (r[1] >= 0 && e->id[0] == 'X' && x != 0 \
+		&& (r[1] = ft_iputstr("0X")) >= 0)
 			r[0] += r[1];
 	}
 	if (r[1] >= 0 && !e->p[1] && (e->fm & 8) == 8 && (e->fm & 4) != 4 && \
--- srcs/ft_wcalc.c
+++ srcs/ft_wcalc.c
@@ -6,7 +6,7 @@
 /*   By: flcarre <flcarre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/19 16:05:35 by flcarre           #+#    #+#             */
-/*   Updated: 2019/02/26 15:57:56 by lutsiara         ###   ########.fr       */
+/*   Updated: 2019/02/26 17:23:26 by flcarre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,9 +47,11 @@ void		ft_wcalc0(t_id *e, unsigned long long x)
 	if (e->id[0] == 'o' && (e->fm & 16) == 16 && e->p[1] \
 	&& e->p[0] > ft_udigitlen(x, 8) && x != 0)
 		e->p[0] -= (e->p[0] >= 1) ? 1 : e->p[0];
-	if ((e->id[0] == 'x' || e->id[0] == 'X' || e->id[0] == 'p') && \
-	(e->fm & 16) == 16 && x != 0)
+	if ((e->id[0] == 'x' || e->id[0] == 'X') && (e->fm & 16) == 16 && x != 0)
 		e->w -= (e->w >= 2) ? 2 : e->w;
+	if (e->id[0] == 'p')
+		e->w -= (e->w >= 2) ? 2 : e->w;
+
 }
 
 void		ft_wcalc1(t_id *e, long double x)
