90b5871a65f4dc951a8fc37fb800799fd3e0f8ae
Florian CARRE <flcarre@student.42.fr>
2019-02-26 16:59:43 +0100

    p

--- srcs/ft_pprocess3.c
+++ srcs/ft_pprocess3.c
@@ -6,7 +6,7 @@
 /*   By: lutsiara <lutsiara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/18 16:22:11 by lutsiara          #+#    #+#             */
-/*   Updated: 2019/02/25 18:19:31 by lutsiara         ###   ########.fr       */
+/*   Updated: 2019/02/26 15:59:04 by lutsiara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ static int	ft_iputfm(t_id *e, unsigned long long x)
 		r[0] += r[1];
 	else if (r[1] >= 0 && (e->fm & 1) == 1 && (r[1] = ft_iputchar('+')) >= 0)
 		r[0] += r[1];
-	if (r[1] >= 0 && (e->fm & 16) == 16 && x != 0)
+	if (r[1] >= 0 && ((e->fm & 16) == 16 || e->id[0] == 'p') && x != 0)
 	{
 		if (e->id[0] == 'o' && (r[1] = ft_iputchar('0')) >= 0)
 			r[0] += r[1];
